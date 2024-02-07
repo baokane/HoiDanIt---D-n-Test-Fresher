2f0d50096005e231a8323b32cf4a125187f71101
tran khanh bao <tranbao22051999@gmail.com>
2024-02-07 09:29:30 +0700

    xong video #24

--- src/component/LayoutAdmin/User/UserTable.jsx
+++ src/component/LayoutAdmin/User/UserTable.jsx
@@ -3,52 +3,7 @@ import { Table } from 'antd';
 import { getFetchListUser } from '../../../services/api';
 import { current } from '@reduxjs/toolkit';
 
-const columns = [
-    {
-        title: 'ID',
-        dataIndex: '_id',
-        // sorter: true,
-        // key: 'ID'
-    },
-    {
-        title: 'Full name',
-        dataIndex: 'fullName',
-        // sorter: true,
-        // key: 'Full name',
-    },
-    {
-        title: 'Email',
-        dataIndex: 'email',
-        // sorter: true,
-        // key: 'Email',
-    },
-    {
-        title: 'Phone',
-        dataIndex: 'phone',
-        // sorter: true,
-        // key: 'Phone',
-    },
-    {
-        title: 'Avatar',
-        dataIndex: 'avatar',
-        // sorter: true,
-        // key: 'Avatar',
-    },
-    {
-        title: 'Action',
-        key: 'fullName',
-        // dataIndex: 'email',
-        // sorter: true,
-        render: function (text, record, index) {
-            return (
-                <>
-                    <button>DELETE</button>
-                    <button>UPDATE</button>
-                </>
-            )
-        }
-    },
-];
+
 
 let data = [
     {
@@ -81,22 +36,67 @@ let data = [
 
 
 const UserTable = () => {
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: '_id',
+            // sorter: true,
+            // key: 'ID'
+        },
+        {
+            title: 'Full name',
+            dataIndex: 'fullName',
+            // sorter: true,
+            // key: 'Full name',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            // sorter: true,
+            // key: 'Email',
+        },
+        {
+            title: 'Phone',
+            dataIndex: 'phone',
+            // sorter: true,
+            // key: 'Phone',
+        },
+        // {
+        //     title: 'Avatar',
+        //     dataIndex: 'avatar',
+        //     // sorter: true,
+        //     // key: 'Avatar',
+        // },
+        {
+            title: 'Action',
+            key: 'fullName',
+            // dataIndex: 'email',
+            // sorter: true,
+            render: function (text, record, index) {
+                return (
+                    <>
+                        <button>DELETE</button>
+                        <button>UPDATE</button>
+                    </>
+                )
+            }
+        },
+    ];
 
-    const [listUser, setListUser] = useState('')
-    const [totalUsers, setTotalUsers] = useState(0)
+    const [listUser, setListUser] = useState([])
+    const [total, setTotal] = useState(0)
     const [current, setCurrent] = useState(1)
     const [pageSize, setPageSize] = useState(2)
 
     const onChange = (pagination, filters, sorter, extra) => {
         console.log('params', pagination, filters, sorter, extra);
-        if (current !== pagination && pagination.current) {
+        if (pagination && pagination.current !== current) {
             setCurrent(pagination.current)
-            console.log(current)
         }
-        // if (pageSize !== pagination && pagination.pageSize) {
-        //     setPageSize(pagination.pageSize)
-        //     setCurrent(1)
-        // }
+        if (pagination && pagination.pageSize !== pageSize) {
+            setPageSize(pagination.pageSize)
+            setCurrent(1)
+        }
     };
 
     useEffect(() => {
@@ -108,7 +108,7 @@ const UserTable = () => {
         console.log('>>>>res:', res)
         if (res && res.data) {
             setListUser(res.data.result)
-            setTotalUsers(res.data.meta.total)
+            setTotal(res.data.meta.total)
         }
     }
 
@@ -119,7 +119,7 @@ const UserTable = () => {
             onChange={onChange}
             rowKey='_id'
             pagination={{
-                current: current, pageSize: pageSize, showSizeChanger: true, total: totalUsers
+                current: current, pageSize: pageSize, showSizeChanger: true, total: total
             }}
         />)
 }
